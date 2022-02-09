da6dc399f1eb11e611a55ecc0bcc772d949bb305
Gregory Vincent <52324737+GregV-crypto@users.noreply.github.com>
2022-02-08 20:11:50 -0500

    Update Node.java

--- 311/FirstProject/Node.java
+++ 311/FirstProject/Node.java
@@ -1,11 +1,12 @@
+
 /**
  * @author Gregory Vincent
  * @date 1/26/22
  * Contains a generic element, reference to left and right childw
  */
-public class Node<T> {
+public class Node<T extends Comparable>{
     
-    public T element; //create a generic element
+    private T element; //create a generic element
     private Node<T> left; //left child
     private Node<T> right; //right child
 
@@ -14,15 +15,12 @@ public class Node<T> {
 
     }
 
-    @overload
-    public Node(T element, Node<T>left, Node right){
+    public Node(T element){
         this.element = element;
-        this.left = left;
-        this.right = right;
     }
     //getter and setter for node elements
     public T getElement(){
-        return this.element;
+        return element;
     }
 
     public void setElement(T element){
@@ -31,16 +29,17 @@ public class Node<T> {
 
     //getters and setters for nodes
     public Node<T> getLeft(){
-        return this.left;
+        return left;
     }
 
     public Node<T> getRight(){
-        return this.right;
+        return right;
     }
 
     public void setLeft(Node<T> left){
         this.left = left;
     }
+
 
     public void setRight(Node<T> right){
         this.right = right;
@@ -48,12 +47,41 @@ public class Node<T> {
 
     @Override
     public String toString(){
-        return this.element.toString() + " " + this.left.toString() + " " + this.right.toString();
+        return this.element.getClass().getSimpleName() + " " + 
+        this.left.getClass().getSimpleName() + " " + this.right.getClass().getSimpleName();
     }
 
-    @Override
-    public void equals(){
-        return;
+    /**
+     * @param firstRef 
+     * @param secondRef - both parameters represent any data type
+     * @return boolean
+     */
+    private boolean helperEquals(Node<T> firstRef, Node<T> secondRef){
+        if(firstRef == secondRef){
+            System.out.println("Nodes are equal.");
+            return true;
+        }
+        if(firstRef == null || secondRef == null){
+            System.out.println("Nodes cannot be equal if null.");
+            return false;
+        }
+        //Return an equality check for each pair of nodes in each tree
+        return firstRef.getElement().equals(secondRef.getElement()) &&
+            firstRef.getLeft().getElement().equals(secondRef.getLeft().getElement()) &&
+            firstRef.getRight().getElement().equals(secondRef.getRight().getElement());
     }
 
+    /**
+     * @return boolean
+     * @param testNode
+     * Checks equality of two different objects
+     */
+    public boolean equals(Node<T> testNode){
+        if(testNode.getClass() != this.getClass()){
+            System.out.println("Object isn't a node.");
+            return false;
+        }
+        return helperEquals(testNode, this);
+    }
 }
+
